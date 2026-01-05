f2a7ff8e65bfcb95d259d818baf27d1636d292f5
João Ramos <joao.pramos@stone.com.br>
2026-01-05 19:02:17 -0300

    checkpoint

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     size = function(term)
       if term.direction == "horizontal" then
-        return vim.o.lines * 0.35
+        return vim.o.lines * 0.5
       elseif term.direction == "vertical" then
         return vim.o.columns * 0.4
       end
